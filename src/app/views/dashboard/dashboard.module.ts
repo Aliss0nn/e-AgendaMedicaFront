aff9239103ab78ce73f9b987893c583bc26a6254
Alisson  Scopel <alli_scopel@hotmail.com>
2023-11-25 23:17:42 -0300

    dashboard e listagem

--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PacienteRoutingModule } from '../paciente/paciente.routing.module';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
   ]
 })
 export class DashboardModule { }
